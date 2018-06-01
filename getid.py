4c174f08410f7774b0114131b6b890fe499d25fd
oyakata29 <ton9928.oyakata@gmail.com>
2018-06-01 23:08:00 +0900

    post cui版　＆ テスト用 getid.py

--- getid.py
+++ getid.py
@@ -3,12 +3,21 @@ from flask import Flask,request
 
 app = Flask(__name__)
 
-@app.route('/test',methods=['POST'])
+@app.route('/id_serch',methods=['POST'])
 def test():
     Student_id = request.form['card_id']
     print (Student_id)
-    return Student_id
+    token = {"token":Student_id}
+    return json.dumps(token)
 
+@app.route('/pw_serch',methods=['POST'])
+def test2():
+    word = request.json['pass']
+    #word = json.loads(passworrd)
+    print(word)
+    print("YAAA")
+    res = {"stat":"OK"}
+    return json.dumps(res)
 if __name__ == '__main__':
     app.debug = True
     app.run(host='00.00.00.00', port=8080)
--- postid.py
+++ postid.py
@@ -1,25 +1,72 @@
-
+import hashlib 
 import nfc
 import binascii
-
-#sys.path.append('/usr/local/src/nfcpy')
-import nfc
 import requests
+import json
+from getpass import getpass
 
+
+token = "null"
 
 def connected(tag):
-    print tag
-    id =  str(tag.idm).encode("hex")  
-    postid = {"id":id}
-    print postid
-    try:
-        #headers = {'Content-Type' : 'utf-8'}
-        r = requests.post("http://rums.rd.dendai.ac.jp/home/oyakata/RUMS/getid.py",data = postid)
-        print r.text
-    except:
-        print "ERROR"
+ print tag
+ id =  str(tag.idm).encode("hex")  
+ postid = {"card_id":id}
+ print postid
+ try:
+  r = requests.post("http://133.14.14.69:8080/id_serch",postid)
+  global token
+  token = r
+ except:
+  print "ERROR"
 
-clf = nfc.ContactlessFrontend('usb')
-clf.connect(rdwr={'on-connect': connected})
-clf.close()
+"""pass input"""
+def input_pw(id):
+ #printf"input password"
+ solt = json.loads(token.text)
+ #solt = token
+ print "solt"
+ print solt["token"]
+ while(1):
+  password = getpass('input password: ')
+  password += solt["token"]
+  s_hash = hashlib.sha256(password).hexdigest()
+  #print s_hash
+  post_h = {'pass':s_hash,'card_id':id}
+  post_h = json.dumps(post_h)
+  #print post_h
+  headers = {'content-type': 'application/json'}
+  r = requests.post("http://133.14.14.69:8080/pw_serch",post_h,headers = headers) 
+
+  print "pass_request"
+  print r.text
+  r = json.loads(r.text)
+  if r["stat"] == 'OK':
+   return 1
+  else:
+   print "false pass" 
+
+
+
+if __name__ == '__main__':
+
+ while(1):
+  clf = nfc.ContactlessFrontend('usb')
+  card = clf.connect(rdwr={'on-connect': connected})
+  clf.close()
+
+  id =  str(card.idm).encode("hex") 
+
+  app = 0
+  print token.text
+  stat = token.status_code
+  if stat == 200:
+   app =  input_pw(id)
+  elif stat == 403:
+   print "this card is no use"
+ 
+  if app == 1:
+   print "sacses"
+  else: 
+   print "OH,ERROR"
 
