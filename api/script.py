4f90148a7c50b593377999e259b8f31f595e4404
kawarai-WSL <16rd044@ms.dendai.ac.jp>
2018-06-02 16:05:31 +0900

    apiサーバver1.0 :ログインの処理完成

--- api/script.py
+++ api/script.py
@@ -13,3 +13,12 @@ def get_token(key):
     salt = str(datetime.now().strftime("%Y/%m/%d %H")+key)
     ret_token = hashlib.sha256(salt.encode()).hexdigest()
     return ret_token
+
+def make_hash_of_synthesized_str(*str_array):
+    synthesis_str = "" 
+    for i in range(len(str_array)):
+        synthesis_str += str_array[i]
+    #return hashlib.sha256(synthesis_str.encode()).hexdigest()
+    ret = hashlib.sha256(synthesis_str.encode()).hexdigest()
+    print(ret)
+    return ret
--- api/server.py
+++ api/server.py
@@ -12,36 +12,75 @@ app.secret_key = 'hogehoge'
 
 @app.route('/id_check',methods=['POST'])
 def id_check():
+    # POSTされたIDを取得
     Student_id = request.form['card_id']
+
+    # DB接続(初期化)
     conn = sqlite3.connect(DATABASE)
     cur = conn.cursor()
+
+    # POSTされたIDをDBと照合するための処理
     id = cur.execute('select Student_id from usr_table where Student_id = \'' +Student_id+ '\'')
     response = Response()
+    #IDがあるかないか
     if len(cur.fetchall()) == 0:
         response.status_code = 403
+
+    # ログイン状態取得
+    cur.execute('select inout from usr_table where Student_id = \'' +Student_id + '\'')
+    data = cur.fetchone() # data[0] is inout
+    inout = data[0]
+    
+    if inout == 1: #ログイン状態にあった場合
+        cur.execute('update usr_table set inout = 0 where Student_id = \''+Student_id+'\'')
+        response.status_code = 203
     else:
         response.status_code = 201
         response.data = script.get_token_json(Student_id)
+        # DBに発行したトークンを保存
         cur.execute('update usr_table set token = \''+script.get_token(Student_id)+'\'where Student_id = \''+Student_id+'\'')
     
+    # DBの更新を保存&DBクローズ
     conn.commit()
     cur.close()
-    response.content_type = 'application/json'    
+    
+    response.content_type = 'application/json'
     return response
 
 @app.route('/pw_check',methods=['POST'])
 def pw_check():
-    pw = request.form['passwd']
-    print(pw)
+    # POSTされたjsonの取得
+    pw_msg = request.json
+    # jsonの展開
+    Student_id = pw_msg['card_id']
+    got_pw = pw_msg = pw_msg['passwd']
+    print(Student_id)
+    print(got_pw) 
+    # DB接続(初期化)
     conn = sqlite3.connect(DATABASE)
     cur = conn.cursor()
-    id = cur.execute('select Student_id from usr_table where Student_id = \'' +session.get('id') + '\' and passwd = \''+ pw + '\'')
+
+    # POSTされたIDによってpasswardを引き出す
+    cur.execute('select passwd, token from usr_table where Student_id = \'' +Student_id + '\'')
+    data = cur.fetchone() # data[0] is passward. data[1] is token
+    true_pw = data[0]
+    token = data[1]
+    
+    # DBから引き抜いたPWとtokenを合成、ハッシュ化
+    made_hash = script.make_hash_of_synthesized_str(true_pw,token)
+    
+    # パスワードの判定
     response = Response()
-    if len(cur.fetchall()) == 0 or len(cur.fetchall()) > 1:
-        response.status_code = 403
+    if got_pw == made_hash:
+        response.status_code=202
+        # DBのinout_stateを1に
+        cur.execute('update usr_table set inout = 1 where Student_id = \''+Student_id+'\'')
     else:
-        response.status_code = 201
-        session['check'] = True
+        response.status_code = 405
+   
+    # DBの更新を保存&DBクローズ
+    conn.commit()
+    cur.close()
     return response
 
 @app.route('/logout',methods=['POST'])
