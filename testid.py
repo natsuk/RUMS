6dba88869599034feddbc24595c89766dc655283
oyakata29 <ton9928.oyakata@gmail.com>
2018-06-18 13:35:57 +0900

    カードデータの抜き取り

--- testid.py
+++ testid.py
@@ -5,19 +5,28 @@ import binascii
 #sys.path.append('/usr/local/src/nfcpy')
 import nfc
 
-service_code = 0x0109
+service_code = 0xfc49
 
 def connected(tag):
     print tag
-  
     if isinstance(tag, nfc.tag.tt3.Type3Tag):
         try:
-            sc = nfc.tag.tt3.ServiceCode(service_code >> 6, service_code & 0x3f)
-            bc = nfc.tag.tt3.BlockCode(2, service=0)
-            print "block: %s" % binascii.hexlify(tag.read_without_encryption([sc], [bc]))
-            print binascii.b2a_uu(tag.read_without_encryption([sc], [bc]))
-        except Exception as e:                                                                                                                                                                                                                                              
-            print "error: %s" % e
+         i = 0
+         f = open(str(service_code),"wb")
+         while(1):
+             sc = nfc.tag.tt3.ServiceCode(service_code >> 6, service_code & 0x3f)
+             bc = nfc.tag.tt3.BlockCode(i, service=0)
+             data = tag.read_without_encryption([sc], [bc])
+             #print(data)
+             
+             f.write(data)
+
+             print "block: %s" % binascii.hexlify(tag.read_without_encryption([sc], [bc]))
+             print binascii.b2a_uu(tag.read_without_encryption([sc], [bc]))
+             i += 1
+        except Exception as e:  
+         print "error: %s" % e
+         f.close
     else:   
         print "error: tag isn't Type3Tag"
         
